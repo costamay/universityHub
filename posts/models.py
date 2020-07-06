a481256c44fac2f760b8753394d8e1105834b21a
Margaret mwangi <maggiemwas91@gmail.com>
2020-07-06 10:55:14 +0300

    Add all

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Home(models.Model):
-    header = models.CharField(max_length=60)
-    description = models.TextField()
-    home_image = models.ImageField()
+# class Home(models.Model):
+#     header = models.CharField(max_length=60)
+#     description = models.TextField()
+#     home_image = models.ImageField()
