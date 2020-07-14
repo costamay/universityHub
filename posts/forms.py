9c1cdb92936bddd04aac01e354f1c6c7b18b8263
Margaret mwangi <maggiemwas91@gmail.com>
2020-07-14 23:07:20 +0300

    Fix errors

--- posts/forms.py
+++ posts/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
         fields = ('project_name', 'project_picture', 'project_description', 'project_category', 'project_url')
         
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={
+    description = forms.CharField(widget=forms.Textarea(attrs={
         'class': 'form-control',
         'placeholder': 'Type your comment',
         'id': 'username',
